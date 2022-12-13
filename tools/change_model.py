eaaa14d1afe4ca9346332caf523b39f48983ff6c
Zijian Zhou <franciszzj@gmail.com>
2022-12-13 17:16:04 +0000

    update tools/change_model

--- tools/change_model.py
+++ tools/change_model.py
@@ -8,10 +8,13 @@ if __name__ == '__main__':
     new_model = copy.deepcopy(model)
     for key, value in model['state_dict'].items():
         if 'panoptic_head.transformer_decoder' in key:
+            global_key = key.replace(
+                'panoptic_head.transformer_decoder', 'panoptic_head.global_transformer_decoder')
             high2low_key = key.replace(
                 'panoptic_head.transformer_decoder', 'panoptic_head.high2low_transformer_decoder')
             low2high_key = key.replace(
                 'panoptic_head.transformer_decoder', 'panoptic_head.low2high_transformer_decoder')
+            new_model['state_dict'][global_key] = value
             new_model['state_dict'][high2low_key] = value
             new_model['state_dict'][low2high_key] = value
-    torch.save(new_model, model_name.replace('.pth', '_high_low.pth'))
+    torch.save(new_model, model_name.replace('.pth', '_global_high_low.pth'))
